f603c8f291a39c3cb058679ebbc378dbe7ca1eb3
SindhuReddy KalathurGopal <51187172+sindhu000018@users.noreply.github.com>
2020-05-14 01:01:43 +0530

    Update FB_scrape.py

--- FB_scrape.py
+++ FB_scrape.py
@@ -1,11 +1,19 @@
 !pip install facebook-scraper
 import pandas as pd
-fb_data=[]
-from facebook_scraper import get_posts
-for post in get_posts('FactlyIndia'):     
-  temp_dict=dict(post)
-  fb_data.append(temp_dict)
+from datetime import datetime
+
+groupLists=['FactlyIndia','TimsFactCheck','QuintFactCheck']
+fb_data=[] 
+dateTime = datetime.now()
+fileName = 'data_fb_'+str(dateTime)+'.csv'
+print(fileName)
+for item in groupLists:
+  for post in get_posts(item, pages =20):     
+    temp_dict=dict(post)
+    fb_data.append(temp_dict)
+    # fb_data.append(item)
   # print(tweet)
 fb_df = pd.DataFrame(fb_data)
+fb_df['scrapedDate'] = dateTime
 fb_df.head()
-fb_df.to_csv('FactlyIndia_fb.csv')
+fb_df.to_csv(fileName)
